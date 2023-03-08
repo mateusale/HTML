f913ef2601a848707d5e0495c4022b398095a4b4
mateusale <mateuspbmulol@gmail.com>
2023-03-08 15:49:05 -0300

    feat: criação do projeto

---
+++ form/config.php
@@ -0,0 +1,20 @@
+<?php
+
+    $dbHost = 'localhost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName = 'formulario-mateus';
+    
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+
+     if($conexao->connect_errno)
+     {
+         echo "Erro";
+     }
+     else
+     {
+         echo "Conexão efetuada com sucesso";
+     }
+
+
+?>
